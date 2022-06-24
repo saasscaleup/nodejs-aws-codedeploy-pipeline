2d0f51d25c1359b0c8d7b4699c463c713b5b271f
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-24 20:20:57 +0300

    create deploy via github - update logs - 10

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,4 +3,4 @@
 echo 'run application_stop.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
 echo 'killall node' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-killall -q node >> /home/ubuntu/nodejs-server-cicd/deploy.log
+killall -q node
