0aa912255389cd64baa472c86a0af56b8e338db0
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-24 20:34:55 +0300

    create deploy via github - update logs - 15

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,8 +4,8 @@ echo 'run after_install.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 echo 'killall node' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 if pgrep node; then killall node; fi >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
-echo 'update ubuntu permissions' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-chown ubuntu:ubuntu -R /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
+# echo 'update ubuntu permissions' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+# chown ubuntu:ubuntu -R /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
 echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 cd /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
