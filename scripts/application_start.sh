0b3393e47b91689964f5dc995ee27b1e9696f76c
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-24 20:30:30 +0300

    create deploy via github - update logs - 13

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,5 +5,5 @@ echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 cd /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
-echo 'app.js > hello.log 2> error.log &' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-node app.js > hello.log 2> error.log & 
+echo 'npm start' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+node start 
--- scripts/application_stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_stop.sh: ' > /home/ubuntu/nodejs-server-cicd/deploy.log
-
-echo 'killall node' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-if pgrep node; then killall node; fi >> /home/ubuntu/nodejs-server-cicd/deploy.log
