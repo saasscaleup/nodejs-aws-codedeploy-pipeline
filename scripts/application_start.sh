41cc831cc84c55a79991cab31f3100dd8093ef45
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-24 20:18:00 +0300

    create deploy via github - update logs - 9

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,5 +5,5 @@ echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 cd /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
-echo 'index.js > hello.log 2> error.log &' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-node index.js > hello.log 2> error.log & 
+echo 'app.js > hello.log 2> error.log &' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+node app.js > hello.log 2> error.log & 
