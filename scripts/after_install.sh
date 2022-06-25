9cbd67e20c4a186f1afa2f37f9043d70a5585942
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-25 09:00:29 +0300

    create deploy via github - update logs - 31

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-server-cicd/deploy.log
 
 # echo 'update ubuntu permissions' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 # chown ubuntu:ubuntu -R /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
-echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-cd /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-server-cicd/deploy.log
+cd /home/ec2-user/nodejs-server-cicd >> /home/ec2-user/nodejs-server-cicd/deploy.log
 
-echo 'npm install' >> /home/ubuntu/nodejs-server-cicd/deploy.log 
-npm install >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'npm install' >> /home/ec2-user/nodejs-server-cicd/deploy.log 
+npm install >> /home/ec2-user/nodejs-server-cicd/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-server-cicd/deploy.log
 
-echo 'pm2 restart nodejs-server' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-pm2 restart nodejs-app >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'pm2 restart nodejs-server' >> /home/ec2-user/nodejs-server-cicd/deploy.log
+pm2 restart nodejs-app >> /home/ec2-user/nodejs-server-cicd/deploy.log
