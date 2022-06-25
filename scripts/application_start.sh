7c5bd3e99f0395792058bff7c22ca4f062bace82
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-25 08:20:56 +0300

    create deploy via github - update logs - 26

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
 echo 'pm2 restart nodejs-server' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-pm2 restart nodejs-server >> /home/ubuntu/nodejs-server-cicd/deploy.log
+pm2 restart nodejs-app >> /home/ubuntu/nodejs-server-cicd/deploy.log
