189e516b00c5e523a7679c2782445338470ec82c
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-25 08:10:45 +0300

    create deploy via github - update logs - 23

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
-# echo 'update ubuntu permissions' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-# chown ubuntu:ubuntu -R /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'update ubuntu permissions' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+chown ubuntu:ubuntu -R /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
 echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 cd /home/ubuntu/nodejs-server-cicd >> /home/ubuntu/nodejs-server-cicd/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,5 +8,5 @@ echo 'run application_start.sh: ' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 # echo 'killall node app.js' >> /home/ubuntu/nodejs-server-cicd/deploy.log
 # if pgrep node; then killall node; fi >> /home/ubuntu/nodejs-server-cicd/deploy.log
 
-echo 'pm2 restart' >> /home/ubuntu/nodejs-server-cicd/deploy.log
-pm2 restart >> /home/ubuntu/nodejs-server-cicd/deploy.log
+echo 'pm2 restart nodejs-server' >> /home/ubuntu/nodejs-server-cicd/deploy.log
+pm2 restart nodejs-server >> /home/ubuntu/nodejs-server-cicd/deploy.log
