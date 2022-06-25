0537cbf106b87a85bb5ecb26dd60934b5f717152
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-25 08:19:27 +0300

    create deploy via github - update logs - 25

--- scripts/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run before_install.sh: '
-
-echo 'rm  /opt/codedeploy-agent/deployment-root/deployment-instructions/*-cleanup'
-rm  /opt/codedeploy-agent/deployment-root/deployment-instructions/*-cleanup
