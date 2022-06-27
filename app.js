8183557a8cdfd0e257653d2ece51cb28e1794d7e
Saas Scale-Up <saas-scale-up@danis-MacBook-Pro.local>
2022-06-27 18:47:11 +0300

    local - one last time because this is wonderful commit

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '1,000,000';
+const version = '2,999,999';
 
 app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: This is Home Page!</h1>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
