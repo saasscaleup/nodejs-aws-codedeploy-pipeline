e7843f839b960a23832e30bbf516375b9b7b786a
aviv-naim <117722386+aviv-naim@users.noreply.github.com>
2023-01-31 16:12:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '2,999,999';
+const version = '3,000,000';
 
 app.get('/', (req, res) => {
     // set response content    
