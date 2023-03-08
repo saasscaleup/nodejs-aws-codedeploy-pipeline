697c9cdfbe02e40c2ca17c84514aec3b1de20b02
Josh Krebs <joddykrebs4@gmail.com>
2023-03-08 11:17:18 -0800

    pm2 again

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 // comment
 
-const version = 'another change with pm2';
+const version = 'pm2 again ';
 
 app.get('/', (req, res) => {
     // set response content
