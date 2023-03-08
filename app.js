50df16cf501ad3ba71565e2ed3a85bb574e20dab
Josh Krebs <joddykrebs4@gmail.com>
2023-03-08 14:56:30 -0800

    add permissions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 // comment
 
-const version = 'test update';
+const version = 'update with permissions';
 
 app.get('/', (req, res) => {
     // set response content
