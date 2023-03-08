409cf73cbe02c0b4f59f2381566994e0107c8c9e
Josh Krebs <joddykrebs4@gmail.com>
2023-03-08 10:09:00 -0800

    local - add github action deploy to aws codeDEploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 // comment
 
-const version = '3';
+const version = '5';
 
 app.get('/', (req, res) => {
     // set response content
