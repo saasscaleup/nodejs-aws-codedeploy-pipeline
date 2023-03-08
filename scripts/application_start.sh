2b944bdcfe6dab12cc21cc91cfe66e5eeb44a708
Josh Krebs <joddykrebs4@gmail.com>
2023-03-08 11:45:18 -0800

    trying with sudo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+sudo pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
