957407d69a4f9b2d355cc537ab459d01da2bd12d
Josh Krebs <joddykrebs4@gmail.com>
2023-03-08 11:03:54 -0800

    with pkill

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,3 +4,4 @@ echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeli
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+sudo pkill node
