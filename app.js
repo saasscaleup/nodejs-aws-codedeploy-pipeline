9c91f580388ccb718aee5605b24271d3c5202d4c
Josh Krebs <joddykrebs4@gmail.com>
2023-03-08 12:39:36 -0800

    permission for scpipts

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 // comment
 
-const version = 'change to give permission to bash scrpots';
+const version = 'giver permopssion to scrpits';
 
 app.get('/', (req, res) => {
     // set response content
