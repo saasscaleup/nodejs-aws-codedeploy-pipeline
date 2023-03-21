69bde059c7cabb2b6031bd1191563cec98dcda9f
jipx <pengxiang00@gmail.com>
2023-03-21 19:12:03 +0800

    commitjipx

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: jipx,THis is AMAZING!!! Like & Subscribe!</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
