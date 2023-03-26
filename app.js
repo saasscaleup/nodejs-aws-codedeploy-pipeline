e9b2b1e200a1bdb9dcb7c0bad28b2399f1429943
jipx <pengxiang00@gmail.com>
2023-03-26 22:33:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = "127.0.0.1"; // Your server ip address
 const port = 3000;
 
-const version = "5.26.1.12.3334";
+const version = "6.26.1.12.3334";
 
 app.get("/", (req, res) => {
   // set response content
